93bfc8669a2bdf434ca2ed1153fc88d8e2d72f8b
Shrinidhi Sridhar <shrinidhisridhar30@gmail.com>
2018-03-06 09:01:56 -0500

    Fix the errors in JS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $('.info-panel-btn').on('click', function () {
   var id = $(this).attr('href');
-  var isOpen = $(this).hasclass('info-panel-btn-active');
+  var isOpen = $(this).hasClass('info-panel-btn-active');
   $('.info-panel').attr('hidden', true);
   $('.info-panel-btn').removeClass('info-panel-btn-active');
   if (!isOpen) {
